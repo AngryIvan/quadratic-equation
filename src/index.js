d8c460d8b99f99f91d2af59a524c49c964c6af28
AngryIvan <leftmetalban@gmail.com>
2018-09-25 12:08:22 +0300

    implemented retrieving numbers from string and calculating equations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    const solutionArray = [];
+    //retrieving a, b, c from equation string
+    const abc = (equation.split('* x'));
+    let a = parseInt(abc[0]);
+    const buffer = abc[1].split('^2');
+    let b = (buffer[1]).split(' ');
+    b = parseInt(b.join('')); //deleting space between minus sign and a number
+    let c = (abc[2]).split(' ');
+    c = parseInt(c.join('')); //deleting space between minus sign and a number
+
+    //calculate
+    let discrSqrt = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+    let x1 = ((-b) + discrSqrt) / (2 * a);
+    let x2 = ((-b) - discrSqrt) / (2 * a);
+    solutionArray.push(Math.round(x1), Math.round(x2));
+
+    return solutionArray.sort((left, right) => left - right);
 }
